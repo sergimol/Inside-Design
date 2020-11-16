90dfa88d4cf769dec2950d70e9c93de401b8c395
Sergio Molinero <sergimol@ucm.es>
2020-11-16 19:57:19 +0100

    Prueba

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ export default class Game extends Phaser.Scene {
   preload() {}
 
   create() {
-    this.add.text(10, 10, "¡Hola, mundo!", { fontColor: 0xffff00 });
+    this.add.text(10, 10, "+ el apellido de tu madre", { fontColor: 0xffff00 });
   }
 
   update(time, delta) {}
