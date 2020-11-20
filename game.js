994eb1e889b5fed5b8cb630ef0d02aa0c7c90856
Andrés Carnicero <acarni01@ucm.es>
2020-11-20 18:09:43 +0100

    Pruebas1

--- game.js
+++ game.js
@@ -2,10 +2,18 @@ export default class Game extends Phaser.Scene {
   constructor() {
     super({ key: "main" });
   }
-  preload() {}
+  preload() {
+
+    this.preload.image('phaser', "Sprites/character1.png");
+  }
 
   create() {
+    
+    player = scene.add.sprite(100, 200, 'phaser');
+
     this.add.text(10, 10, "Estoy hecho un puto, amono", { fontColor: 0xffff00 });
+    scene.load(player);
+    
   }
 
   update(time, delta) {
