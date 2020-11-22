ece726235d0e6f1a9a7f59131bb7e3b38d02c919
Andrés Carnicero <acarni01@ucm.es>
2020-11-22 01:51:15 +0100

    Escenario bien

--- game.js
+++ game.js
@@ -5,43 +5,92 @@ export default class Game extends Phaser.Scene {
   
   preload() {
     this.load.spritesheet('sky', './Sprites/character1.png', {frameWidth: 24, frameHeight: 24});
-    this.load.image('hummus', 'Sprites/hummardo.jpg');
-    this.load.image('platform', 'Sprites/platform.png');
+    this.load.image('hummus', './Sprites/BACKGROUND.png'); //nuevo
+    this.load.image('platform', './Sprites/platform.png');
+
+    //nuevo
+    this.load.image('Wall', './Sprites/Wall.png');
+    this.load.image('upWall', './Sprites/upWall.png');
+    this.load.image('downWall', './Sprites/downWall.png');
+    this.load.image('cobertura', './Sprites/Cobertura.png');
+
+
   }
   
   create() {
 
-    let platforms;
+    //let platforms;
     this.add.image(700, 400, 'hummus');
 
     //Grupo de plataformas
+    
+    /*
     this.platforms = this.physics.add.staticGroup();
     this.platforms.create(400, 500, 'platform');
     this.platforms.create(900, 300, 'platform');
     this.platforms.create(700, 700, 'platform').setScale(3).refreshBody();
+    */
     this.player = this.physics.add.sprite(100, 450, 'sky');
     this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true); //esto no esta funcionando profe
+    
+    //Grupo de coberturas
+    let cobers;
+    this.cobers = this.physics.add.staticGroup();
+    this.cobers.create(577, 591, 'cobertura');
+    this.cobers.create(903, 310, 'cobertura');
+    this.cobers.create(257, 246, 'cobertura');
+
+    
+
+    //Muro Arriba
+    let upWall;
+    this.upWall = this.physics.add.staticGroup();
+    this.upWall.create(700, 49, 'upWall');
+
+    //Muro Arriba
+    let UpWall;
+    this.UpWall = this.physics.add.staticImage(700, 49, 'upWall');
+
+    //Muro Abajo
+    let DownWall;
+    this.DownWall = this.physics.add.staticImage(700, 770, 'downWall');
+    this.DownWall.setFlipY(true);
+
+    //Muro 
+    let wall;
+    this.wall = this.physics.add.staticGroup();
+    this.wall.create(30, 400, 'Wall');
+    this.wall.create(1370, 400, 'Wall');
+
+
+
+
   
     //ANIMACIONES
     this.anims.create({
-      key:'right',
-      frames: this.anims.generateFrameNumbers('sky', {start: 4, end: 15}),
-      frameRate: 10,
-      repeat: -1
-    })
-    this.anims.create({
-      key:'left',
+      key:'walk',
       frames: this.anims.generateFrameNumbers('sky', {start: 4, end: 15}),
       frameRate: 10,
       repeat: -1
     })
 
-    this.physics.add.collider(this.player, this.platforms);
+
+    //Collider y escala personaje
+    //this.physics.add.collider(this.player, this.platforms);
+    this.physics.add.collider(this.player, this.cobers);
+    this.physics.add.collider(this.player, this.UpWall);
+    this.physics.add.collider(this.player, this.DownWall);
+    this.physics.add.collider(this.player, this.wall);
+
+
+
     this.player.setScale(5);
 
     //this.player.setVelocityX(-160);
     this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.player.setSize(14,15);
 
   }
 
@@ -51,14 +100,14 @@ export default class Game extends Phaser.Scene {
     {
       this.player.setFlipX(true)
       this.player.setVelocityX(-160);
-      this.player.anims.play('right', true);
+      this.player.anims.play('walk', true);
       
     }
     else if (this.cursors.right.isDown)
     {
       this.player.setFlipX(false);
       this.player.setVelocityX(160);
-      this.player.anims.play('right', true);
+      this.player.anims.play('walk', true);
       
     }
     else
@@ -79,63 +128,4 @@ export default class Game extends Phaser.Scene {
     }
     
   }
-  /*
-  preload() {
-
-    //this.load.spritesheet('phaser', 'Sprites/character1.png', 120, 144, 30);
-    //Carga de recursos
-    this.load.image('hummus', 'Sprites/hummardo.jpg');
-    this.load.image('platform', 'Sprites/platform.png');
-    this.load.spritesheet('player', './Sprites/character1.png', {frameWidth: 24, frameHeight: 24});
-
-  }
-
-
-  create() { 
-
-    let platforms;
-
-    this.add.text(10, 10, "Estoy hecho un puto, amono", { fontColor: 0xffff00 });
-    //var phaser=this.add.sprite(400, 400, "phaser")
-    //movida nueva
-    this.add.image(700, 400, 'hummus');
-
-    //Grupo de plataformas
-    this.platforms = this.physics.add.staticGroup();
-    this.platforms.create(400, 500, 'platform');
-    this.platforms.create(900, 300, 'platform');
-    this.platforms.create(700, 700, 'platform').setScale(3).refreshBody();
-
-    //Player
-    this.player = this.physics.add.sprite(100, 450, 'player');
-    this.player.setBounce(0.2);
-    this.player.setColliderWorldBounds(true);
-
-    
-    this.anims.create({
-      key:'right',
-      frames: this.anims.generateFrameNumbers('dude', {start: 4, end: 15}),
-      frameRate: 10,
-      repeat: -1
-    })
-    this.anims.create({
-      key:'turn',
-      frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 0}),
-      frameRate: 20
-    })
-    this.anims.create({
-      key:'left',
-      frames: this.anims.generateFrameNumbers('dude', {start: 4, end: 15}),
-      frameRate: 10,
-      repeat: -1
-    })
-     
-    //this.physics.add.collider(player, platforms);
-    
-
-
-    
-  }
-  */
-
 }
