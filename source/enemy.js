e6cdd1d98196d996c2f4b31826a3f91fc0acdf7f
javimu06 <javimu06@ucm.es>
2020-11-25 15:03:32 +0100

    Fallo tecnico
    a ver si asi se arregla

---
+++ source/enemy.js
@@ -0,0 +1,34 @@
+/*import Humanoid from "/source/humanoid.js";
+export default class Enemy extends Humanoid{
+    constructor(scene, x, y, Sprite){
+        super(scene, x, y, '______');
+
+        //Atributos
+
+
+        /////////////
+        //Animaciones
+        const anims = scene.anims;
+
+        anims.create({
+            key:'walk',
+            frames: anims.generateFrameNumbers(this.Sprite, {start: 4, end: 9}), //15
+            frameRate: 15,
+            repeat: -1
+        })
+        anims.create({
+        key:'idle',
+        frames: anims.generateFrameNumbers(this.Sprite, {start: 1, end: 3}),
+        frameRate: 7,
+        repeat: -1
+        })
+
+        
+        
+    }//Fin constructora
+    Update(){
+        ///////////
+        //MOVEMENT - IA
+        
+    }
+*/
