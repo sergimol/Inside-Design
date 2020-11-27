b9037ac82666d0383f004161fbd2a16c216b3cb8
diecasti <diecasti@ucm.es>
2020-11-27 14:06:59 +0100

    ya esta my in side demons have been drowne
    but they know how to swim

--- game.js
+++ game.js
@@ -159,7 +159,6 @@ this.puntero = new Puntero(this, 400, 300);
     
 
     //Jugador
-<<<<<<< Updated upstream
     this.player.update();
     //Enemigos
     if(!this.enemy.isDead){
@@ -170,8 +169,6 @@ this.puntero = new Puntero(this, 400, 300);
         child.update();
       }
     });
-=======
     this.player.update(this.angleToPointer);
->>>>>>> Stashed changes
   }
 }
