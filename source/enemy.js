4ccda825a6a76f9400aa75c57d317c37255978e2
javimu06 <javimu06@ucm.es>
2020-11-27 17:11:28 +0100

    Movimiento enemigos

--- source/enemy.js
+++ source/enemy.js
@@ -23,29 +23,15 @@ export default class Enemy extends Humanoid{
         repeat: -1
         })*/
 
-        
-        let dir = Math.floor(Math.random()*4);
-        switch(dir){
-            case 0:
-                this.body.setVelocity(0, -this.speed);  //up
-                break;
-            case 1:
-                this.body.setVelocity(-this.speed, 0);  //left
-                break;
-            case 2:
-                this.body.setVelocity(0, this.speed);   //down
-                break;
-            case 3:
-                this.body.setVelocity(this.speed, 0);   //right
-                break;
-            default:
-                break;
-        }
+        this.dirX = -1;
+        this.dirY = 0;
     }//Fin constructora
-    Update(){
+
+    decidirMov(){
         const {speed} = this;
         const enemyBlocked = this.body.blocked;
         if(enemyBlocked.down ||enemyBlocked.up ||enemyBlocked.right ||enemyBlocked.left){
+            console.log("colision");
             let possibleDirections = [];
             for(const direction in enemyBlocked){
                 possibleDirections.push(direction);
@@ -53,27 +39,38 @@ export default class Enemy extends Humanoid{
             const newDirection = possibleDirections[Math.floor(Math.random()*4)+1];
             switch(newDirection){
                 case 'up':
-                    this.body.setVelocity(0,-this.speed);
+                    this.dirX = 0;
+                    this.dirY = -1;
                     break;
                 case 'left':
-                    this.body.setVelocity(-this.speed,0);
+                    this.dirX = -1;
+                    this.dirY = 0;
                     break;
                 case 'down':
-                    this.body.setVelocity(0,this.speed);
+                    this.dirX = 0;
+                    this.dirY = 1;
                     break;
-                case 'up':
-                    this.body.setVelocity(this.speed,0);
+                case 'right':
+                    this.dirX = 1;
+                    this.dirY = 0;
                     break;
                 case 'none':
-                    this.body.setVelocity(0,0);
-                    break;
+                    this.dirX = 0;
+                    this.dirY = 0;
                 default:
                     break;
             }
         }
-        
+    }
+    update(){
+        this.decidirMov();
+        this.move(this.dirX, this.dirY);
     }
 }
+
+
+
+
 
 //EN GAME
 /*
