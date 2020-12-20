6b634f16dd516fccaeff8082feb01a11dad93aa1
javimu06 <javimu06@ucm.es>
2020-12-20 04:23:54 +0100

    Cambio Tileset
    Implementado el cambio de tilesets

--- game.js
+++ game.js
@@ -7,44 +7,50 @@ export default class Game extends Phaser.Scene {
   constructor() {
     super({ key: "main" });
   }
-  
+
   preload() {
-    this.load.spritesheet('player', './Sprites/character1.png', {frameWidth: 24, frameHeight: 24});
+    this.load.spritesheet('player', './Sprites/character1.png', { frameWidth: 24, frameHeight: 24 });
 
     //Diego
-    this.load.spritesheet('bullet', 'Sprites/bullet2.png', {frameWidth: 64, frameHeight: 64});
+    this.load.spritesheet('bullet', 'Sprites/bullet2.png', { frameWidth: 64, frameHeight: 64 });
     this.load.image('crosshair', 'Sprites/crosshair.png');
 
     //Javi
+    //Tiles de estéticas
     this.load.image('tiles', './Sprites/tiles/TilesetDEF.png');
-    this.load.tilemapTiledJSON('dungeon','./Sprites/tiles/NivelBase.json');
+    this.load.image('tilesCrash', './Sprites/tiles/TilesetDEFcrash.png');
+
+
+    this.load.tilemapTiledJSON('dungeon', './Sprites/tiles/NivelBase.json');
 
     //nuevo
-    this.load.audio('mainTheme','./audio/main_theme_v1.0.wav');
+    this.load.audio('mainTheme', './audio/main_theme_v1.0.wav');
     this.load.audio('gunShootSound', './audio/gunShoot.wav');
     this.load.image('gunShoot', './Sprites/gunShootProt.png');
-    
+
   }
-  
+
   create() {
     // colisiones tilemap
-    const map = this.make.tilemap({key:'dungeon'})
-    const tileset = map.addTilesetImage('TilesetBase','tiles',16,16,1,2);
+    this.map = this.make.tilemap({ key: 'dungeon' })
+    this.tileset = this.map.addTilesetImage('TilesetBase', 'tiles', 16, 16, 1, 2);
+    //const tilesetCrash = map.addTilesetImage('TilesetBase', 'tilesCrash', 16, 16, 1, 2);
+    //this.mapCrash = map.add(tilesetCrash);
 
-    const groundLayer = map.createStaticLayer('Ground', tileset);
-    const detailsLayer = map.createStaticLayer('Details', tileset);
-    const wallsLayer = map.createStaticLayer('Walls', tileset);
-    const doorsLayer = map.createStaticLayer('Doors', tileset);
-    const collidersLayer = map.createStaticLayer('Colliders', tileset);
-    const colsLayer = map.createStaticLayer('Cols', tileset);
-    const boxLayer = map.createStaticLayer('Box', tileset);
+    const groundLayer = this.map.createStaticLayer('Ground', this.tileset);
+    const detailsLayer = this.map.createStaticLayer('Details', this.tileset);
+    const wallsLayer = this.map.createStaticLayer('Walls', this.tileset);
+    const doorsLayer = this.map.createStaticLayer('Doors', this.tileset);
+    const collidersLayer = this.map.createStaticLayer('Colliders', this.tileset);
+    const colsLayer = this.map.createStaticLayer('Cols', this.tileset);
+    const boxLayer = this.map.createStaticLayer('Box', this.tileset);
 
-    const entityLayer = map.getObjectLayer('Entities').objects
+    const entityLayer = this.map.getObjectLayer('Entities').objects
 
-    doorsLayer.setCollisionByProperty({collide: true});
-    collidersLayer.setCollisionByProperty({collide: true});
-    colsLayer.setCollisionByProperty({collide: true});
-    boxLayer.setCollisionByProperty({collide: true});
+    doorsLayer.setCollisionByProperty({ collide: true });
+    collidersLayer.setCollisionByProperty({ collide: true });
+    colsLayer.setCollisionByProperty({ collide: true });
+    boxLayer.setCollisionByProperty({ collide: true });
 
     this.matter.world.convertTilemapLayer(doorsLayer);
     this.matter.world.convertTilemapLayer(collidersLayer);
@@ -55,75 +61,92 @@ export default class Game extends Phaser.Scene {
 
     //BULLET
     this.anims.create({
-      key:'shot',
-      frames: this.anims.generateFrameNumbers('bullet', {start: 0, end: 3}),
+      key: 'shot',
+      frames: this.anims.generateFrameNumbers('bullet', { start: 0, end: 3 }),
       frameRate: 8,
       repeat: -1
     })
-    
+
     //BULLETS
     this.bullets = this.add.group();
     this.bullets.enableBody = true;
     this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
-    
-    
+
+
     //PUNTERO
     this.input.setDefaultCursor('url(Sprites/crosshair.png), pointer');
     //this.sys.canvas.style.cursor = 'crosshair'
 
-//    this.puntero = new Puntero(this,400, 450);
-    
-     this.angleToPointer;
-     this.input.on('pointermove', function (pointer){
-        this.angleToPointer = Phaser.Math.Angle.Between(this.player.x, this.player.y, (pointer.x/this.cameras.main.zoom) + this.cameras.main.worldView.x, (pointer.y/this.cameras.main.zoom) + this.cameras.main.worldView.y);
-        
-      }, this);
+    //    this.puntero = new Puntero(this,400, 450);
 
-      //Prototipo Musica
-      //let sound = this.sound.add('mainTheme');
-      //sound.play(); 
-       
-       
-      //CARGA DE OBJETOS
-      this.loadObjects(map);
+    this.angleToPointer;
+    this.input.on('pointermove', function (pointer) {
+      this.angleToPointer = Phaser.Math.Angle.Between(this.player.x, this.player.y, (pointer.x / this.cameras.main.zoom) + this.cameras.main.worldView.x, (pointer.y / this.cameras.main.zoom) + this.cameras.main.worldView.y);
 
-      //Camara
-      this.cameras.main.zoom = 3;
-      this.cameras.main.startFollow(this.player.puntero.intermedio);
+    }, this);
 
+    //Prototipo Musica
+    //let sound = this.sound.add('mainTheme');
+    //sound.play(); 
+
+
+    //CARGA DE OBJETOS
+    this.loadObjects(this.map);
+
+    //Camara
+    this.cameras.main.zoom = 3;
+    this.cameras.main.startFollow(this.player.puntero.intermedio);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.tilemapState = 0;
+    this.input.keyboard.on('keydown-SHIFT', function (event) {
+      if (this.tilemapState == 0) this.tilemapState++;
+      else this.tilemapState--;
+      console.log(this.tilemapState);
+    },this);
   }//End of create
 
-  loadObjects(map){
-    this.enemies = this.add.group();
-      
-      for (const objeto of map.getObjectLayer('Entities').objects) {
-        // `objeto.name` u `objeto.type` nos llegan de las propiedades del
-        // objeto en Tiled
-        if (objeto.name === 'player') {
-          this.player = new Player(this, objeto.x, objeto.y,'player')
-        }
-        else if(objeto.name === 'enemy'){
-          const e = new Enemy(this,  objeto.x, objeto.y, 'player', this.player);
-          this.enemies.add(e);
-        }
-      }
-  }
+  changeLayer() {
+    switch (this.tilemapState) {
+      case 0:
+        this.tileset = this.map.addTilesetImage('TilesetBase', 'tiles', 16, 16, 1, 2);
+        break;
 
-  destroyBullet(b){
-    console.log('bullet hit');
-    b.destroy();
-  }
-
-
-
-  update() {  
- //Jugador
-  //Enemigos
-  /*this.enemies.children.iterate((child)=>{
-    if(!child.isDead){
-      child.update(this.player);
+      case 1:
+        this.tileset = this.map.addTilesetImage('TilesetBase', 'tilesCrash', 16, 16, 1, 2);
+        break;
     }
-  });*/
-  //console.log(this.angleToPointer);
+  }
+
+  loadObjects(map) {
+    this.enemies = this.add.group();
+
+    for (const objeto of map.getObjectLayer('Entities').objects) {
+      // `objeto.name` u `objeto.type` nos llegan de las propiedades del
+      // objeto en Tiled
+      if (objeto.name === 'player') {
+        this.player = new Player(this, objeto.x, objeto.y, 'player')
+      }
+      else if (objeto.name === 'enemy') {
+        const e = new Enemy(this, objeto.x, objeto.y, 'player', this.player);
+        this.enemies.add(e);
+      }
+    }
+  }
+
+
+
+  update() {
+    this.changeLayer();
+
+    //Jugador
+    //Enemigos
+    /*this.enemies.children.iterate((child)=>{
+      if(!child.isDead){
+        child.update(this.player);
+      }
+    });*/
+    //console.log(this.angleToPointer);
   }
 }
