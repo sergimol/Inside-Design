152767f96960505697d29520aa4c562bad05ddd6
diecasti <diecasti@ucm.es>
2020-12-28 18:10:52 +0100

    angulo arregaldo para javi

--- source/item.js
+++ source/item.js
@@ -2,9 +2,9 @@ export default class Item extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, sprite, player, label,cantidad) {
         super(scene, x, y, sprite);
         this.depth = 3;
+        this.setSize(1,1);
         this.scene.matter.add.gameObject(this);
         this.scene.add.existing(this);
-
         this.body.label = label;
         //this.setScale(4);
         //Referencia al player
@@ -13,8 +13,10 @@ export default class Item extends Phaser.GameObjects.Sprite {
         //this.dir.normalize();
     }
     preUpdate(){
-        let angle = Phaser.Math.Angle.Between(this.body, Phaser.Math.Angle.Between(0,0, this.playerRef.x, this.playerRef.y));
-        this.setAngle(this.body, angle);
+        
+        let angle = Phaser.Math.Angle.Between(this.x,this.y, this.playerRef.x, this.playerRef.y);
+        this.scene.matter.body.setAngle(this.body, angle);
+        //this.scene.matter.body.setAngle(this.body, Phaser.Math.Angle.Between(0,0, this.body.velocity.x, this.body.velocity.y));
         console.log(this.angle)
         //this.thrust(0.0001);
         //this.scene.matter.body.applyForce(this.body,this.angle, 0.003);
