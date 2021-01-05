14f952fa0036ecc547c9ad126bfb89d300181fd4
diecasti <diecasti@ucm.es>
2021-01-05 15:56:20 +0100

    corregido Enemigos mueren dos veces con lanzagranadas etc

--- source/humanoid.js
+++ source/humanoid.js
@@ -50,57 +50,60 @@ export default class Humanoid extends Phaser.GameObjects.Container { //Container
     }//Fin constructora
 
     damage(damagePoints) {
-        this.hitState = true;
-        this.health -= damagePoints;
-        if (this.health <= 0) {
 
-            let sound = this.scene.sound.add('deadSound');
-            sound.setVolume(1.5);
-            sound.play();
+        if (!this.isDead){
+            this.hitState = true;
+            this.health -= damagePoints;
+            if (this.health <= 0) {
 
-            this.isDead = true;
-            this.hitState = false;
+                let sound = this.scene.sound.add('deadSound');
+                sound.setVolume(1.5);
+                sound.play();
 
-            this.weapon.setVisible(false);
-            //console.log('entityDep');
-            this.aspecto.play('enemyDep', true);
+                this.isDead = true;
+                this.hitState = false;
 
-            if (this.body.label === 'enemy') {
-                this.scene.enemyCount--;
-                let numItems = Phaser.Math.RND.between(0, 4);
-                //console.log(numItems);
-                for (let n = 0; n < numItems; ++n) {
-                    let chooseItem = Phaser.Math.RND.between(0, 4);
-                    if (chooseItem < 4)
-                        var item = new Item(this.scene, this.x, this.y, 'bulletAmmo', this.scene.player);
-                    else
-                        var item = new Item(this.scene, this.x, this.y, 'medkit', this.scene.player);
+                this.weapon.setVisible(false);
+                //console.log('entityDep');
+                this.aspecto.play('enemyDep', true);
+
+                if (this.body.label === 'enemy') {
+                    this.scene.enemyCount--;
+                    let numItems = Phaser.Math.RND.between(0, 4);
+                    //console.log(numItems);
+                    for (let n = 0; n < numItems; ++n) {
+                        let chooseItem = Phaser.Math.RND.between(0, 4);
+                        if (chooseItem < 4)
+                            var item = new Item(this.scene, this.x, this.y, 'bulletAmmo', this.scene.player);
+                        else
+                            var item = new Item(this.scene, this.x, this.y, 'medkit', this.scene.player);
+                    }
+                    //Decrementa en 1 EnemyCountDoor de la sala en la que se encuentra el enemigo
+                    --this.doorRef.EnemyCountDoor[this.doorNum];    //El -1 es porque la puerta inicial es 1, pero el array empieza en 0
+                    console.log(this.doorRef.EnemyCountDoor[this.doorNum]);
+
+                    this.body.collisionFilter = {
+                        'group': -2,
+                        'category': 4,
+                        'mask': 1, //mundo y balas jugador
+                        //'group':2,  //asi no colisionan entre si si tienen este mismo valor en negativo, en positivo siempre colisionaran si tienen el mismo valor, con 0 npi, explotara supongo
+                    };
+
                 }
-                //Decrementa en 1 EnemyCountDoor de la sala en la que se encuentra el enemigo
-                --this.doorRef.EnemyCountDoor[this.doorNum];    //El -1 es porque la puerta inicial es 1, pero el array empieza en 0
-                console.log(this.doorRef.EnemyCountDoor[this.doorNum]);
-
-                this.body.collisionFilter = {
-                    'group': -2,
-                    'category': 4,
-                    'mask': 1, //mundo y balas jugador
-                    //'group':2,  //asi no colisionan entre si si tienen este mismo valor en negativo, en positivo siempre colisionaran si tienen el mismo valor, con 0 npi, explotara supongo
-                };
 
             }
-
+            else {
+                //sonido hit
+                let sound = this.scene.sound.add('hitShootSound');
+                //sound.setVolume(0.1);
+                sound.play();
+            }
+            if (this.body.label === 'player')
+                if (this.isDead)
+                    this.hud.setHealth(0);
+                else
+                    this.hud.setHealth(this.health);
         }
-        else {
-            //sonido hit
-            let sound = this.scene.sound.add('hitShootSound');
-            //sound.setVolume(0.1);
-            sound.play();
-        }
-        if (this.body.label === 'player')
-            if (this.isDead)
-                this.hud.setHealth(0);
-            else
-                this.hud.setHealth(this.health);
     }
 
 
