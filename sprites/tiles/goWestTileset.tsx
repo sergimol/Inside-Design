3287e847748599eac81865f4112d5bfb5bc83cd6
Sergio Molinero <sergimol@ucm.es>
2021-01-22 23:15:05 +0100

    Carpeta sprites 2

---
+++ sprites/tiles/goWestTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.10.30" name="tileSetWestEx" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="1440" columns="48">
+ <image source="tileSetWestEx.png" width="864" height="540"/>
+</tileset>
